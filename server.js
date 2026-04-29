09798eba6b6b0cd26acc24794d35a021ee31e903
ishigakimarui01-cmyk <ishigakimarui01@gmail.com>
2026-04-29 15:40:04 +0900

    Update server.js

--- server.js
+++ server.js
@@ -1,20 +1,9 @@
-const express = require('express');
-const { twiml: { VoiceResponse } } = require('twilio');
-
-const app = express();
-app.use(express.urlencoded({ extended: false }));
-
 app.post('/voice', (req, res) => {
   const twiml = new VoiceResponse();
 
-  // 🔥 最小テスト（重要）
-  twiml.say('こんにちは。テストです。日本語が聞こえていますか？');
+  // 👇これだけにする（最重要テスト）
+  twiml.say('こんにちは。テストです。Hello test.');
 
   res.type('text/xml');
   res.send(twiml.toString());
-});
-
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log('running');
 });
