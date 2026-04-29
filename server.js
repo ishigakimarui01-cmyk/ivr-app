d42dc7478acebd79cf98c05f6688bf4b8a72d8f0
ishigakimarui01-cmyk <ishigakimarui01@gmail.com>
2026-04-29 15:41:42 +0900

    Update server.js

--- server.js
+++ server.js
@@ -1,3 +1,9 @@
+const express = require('express');
+const { twiml: { VoiceResponse } } = require('twilio');
+
+const app = express();
+app.use(express.urlencoded({ extended: false }));
+
 app.post('/voice', (req, res) => {
   const twiml = new VoiceResponse();
 
@@ -6,4 +12,9 @@ app.post('/voice', (req, res) => {
 
   res.type('text/xml');
   res.send(twiml.toString());
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log('running');
 });
